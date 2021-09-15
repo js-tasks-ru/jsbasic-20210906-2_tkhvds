ad57dea863a4b181ea0c48743395da8966287722
Tikhonov Danila <tkhvds@yandex.ru>
2021-09-15 21:39:26 +0300

    добавил задачи 2-ого модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,23 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+
 function sumSalary(salaries) {
-  // ваш код...
+
+  let resultSalaries = 0;
+  
+  for (let prop in salaries){
+
+    if (isFinite(salaries[prop]) && typeof (salaries[prop] == 'number')){
+      resultSalaries += salaries[prop];
+    } 
+  }
+  
+  return resultSalaries;
+
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read: function(a, b){
+    this.NumberA = +a;
+    this.NumberB = +b;
+  },
+  sum: function(){
+    return this.NumberA + this.NumberB;
+  },
+  mul: function(){
+    return this.NumberA * this.NumberB;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
